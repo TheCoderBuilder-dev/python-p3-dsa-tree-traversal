4a0088077c791c2910324e9d966ee6ba03ce359d
TheCoderBuilder-dev <brianinesh@gmail.com>
2025-05-25 16:53:03 +0300

    commit

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,21 @@
 class Tree:
-  def __init__(self, root = None):
+  def __init__(self, root=None):
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    return self._dfs(self.root, id)
+
+  def _dfs(self, node, target_id):
+    if node is None:
+      return None
+
+    if node.get("id") == target_id:
+      return node
+
+    for child in node.get("children", []):
+      found = self._dfs(child, target_id)
+      if found:
+        return found
+
+    return None
+
